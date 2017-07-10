602bab0bd07494afc05266ab0be487b3de54631e
Divyansh <somm14divi@gmail.com>
2017-07-10 12:10:35 +0530

    Weighted Graph support added

--- src/Graph500.jl
+++ src/Graph500.jl
@@ -3,12 +3,17 @@ module Graph500
 
   # Dependencies
   using LightGraphs
+  using SimpleWeightedGraphs
 
   export
   # kronecker_generator
-  kronecker_generator
+  kronecker_generator,
+
+  # kernel_1
+  kernel_1
 
   Graph500
   include("kronecker_generator.jl")
+  include("kernel_1.jl")
 
 end # module
--- src/kernel1.jl
+++
@@ -1,1 +0,0 @@
-##Graph Construction Benchmarking
