951dbfec9f85e8ecd62638be5a1157fa22be4989
Divyansh Srivastava <somm14divi@gmail.com>
2017-07-10 13:19:56 +0530

    Somil55/graph loading (#3)
    * Performance enhancement for kernel1 by converting kronecker to column_major
    
    * fixed tests
    
    * updated tests
    
    * removes 1 dependency

--- src/kernel_1.jl
+++ src/kernel_1.jl
@@ -1,8 +1,2 @@
-function kernel_1(edge_list::kroneckerState)
-  ij  = edge_list.edge
-  ijw = edge_list.weight
-  nv = maximum(ij)
-  g = SimpleWeightedGraph(nv)
-  for i in 1:
-
-end
+# function kernel_1(edge_list::kroneckerState)
+# end
--- src/kronecker_generator.jl
+++ src/kronecker_generator.jl
@@ -16,7 +16,7 @@ References
 function kronecker_generator(SCALE, edgefactor, A=0.57, B=0.19, C=0.19)
     N  = 2^SCALE            # Set number of vertices
     M  = edgefactor * N     # Set number of edges
-    ij = ones(Int, M, 2)    # Create index arrays
+    ij = ones(Int, 2, M)    # Create index arrays
     ijw = rand(Float64, M)  # Generate weights
 
     # Loop over each order of bit
@@ -26,21 +26,21 @@ function kronecker_generator(SCALE, edgefactor, A=0.57, B=0.19, C=0.19)
 
     temp =  @parallel (+) for ib in 1:SCALE
         # Compare with probabilities and set bits of indices.
-        random_bits = falses(M, 2)
-        random_bits[:, 1] = rand(M) .> (ab)
-        random_bits[:, 2] = rand(M) .> ( c_norm.*(random_bits[:, 1]) + a_norm.*.!(random_bits[:, 1]) )
+        random_bits = falses(2, M)
+        random_bits[1, :] = rand(M) .> (ab)
+        random_bits[2, :] = rand(M) .> ( c_norm.*(random_bits[1, :]) + a_norm.*.!(random_bits[1, :]) )
         2^(ib-1).*random_bits
     end
 
-    ij[:, 1:2] += temp
+    ij += temp
 
     # Permute vertex labels
     p = randperm(N)
-    ij[:, 1:2] = p[ij[:, 1:2]]
+    ij[1:2, :] = p[ij[1:2, :]]
 
     # Permute the edge list
     p = randperm(M)
-    ij = ij[p, :]
+    ij = ij[:, p]
     ijw = ijw[p]
 
     return kroneckerState(ij, ijw)
