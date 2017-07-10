059a78008811bee04da91e7ba790ec2c78a57429
Divyansh <somm14divi@gmail.com>
2017-07-10 12:26:16 +0530

    fixed tests

--- test/kronecker_generator.jl
+++ test/kronecker_generator.jl
@@ -1,5 +1,5 @@
 @testset "kronecker_generator" begin
   kg = @inferred kronecker_generator(2,2)
-  @test size(kg.edge) == (8,2)
+  @test size(kg.edge) == (2,8)
   @test length(kg.weight) == (8)
 end
