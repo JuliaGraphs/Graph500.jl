1427dddf05629d318dadfb1937842a146cdc4167
Divyansh <somm14divi@gmail.com>
2017-07-10 22:18:19 +0530

    Kernel_1 coded

--- src/kernel_1.jl
+++ src/kernel_1.jl
@@ -1,2 +1,15 @@
-# function kernel_1(edge_list::kroneckerState)
-# end
+function kernel_1(edge_list::kroneckerState)
+  ij = edge_list.edge
+  ijw = edge_list.weight
+  nv = maximum(ij)
+  ne = size(ij)[2]
+  g = SimpleWeightedGraph(nv)
+
+  @simd for i in 1:ne
+    if ij[1, i] != ij[2, i]
+      @inbounds add_edge!(g, ij[1, i], ij[2, i], ijw[i])
+    end
+  end
+
+  return g
+end
