ecb12fc6cb889fa78658610a659c594befb0792f
Divyansh <somm14divi@gmail.com>
2017-07-10 12:25:49 +0530

    Performance enhancement for kernel1 by converting kronecker to column_major

--- src/kernel_1.jl
+++ src/kernel_1.jl
@@ -1,8 +1,8 @@
 function kernel_1(edge_list::kroneckerState)
-  ij  = edge_list.edge
-  ijw = edge_list.weight
-  nv = maximum(ij)
-  g = SimpleWeightedGraph(nv)
-  for i in 1:
+  # ij  = edge_list.edge
+  # ijw = edge_list.weight
+  # nv = maximum(ij)
+  # g = SimpleWeightedGraph(nv)
+  # for i in 1:
 
 end
