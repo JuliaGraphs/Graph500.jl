33930821973f3d010ce2c7a5f6adc722f8b87d0b
Divyansh <somm14divi@gmail.com>
2017-07-21 13:06:13 +0530

    Kernel1 self loop bug

--- src/kernel_1.jl
+++ src/kernel_1.jl
@@ -2,20 +2,29 @@ function kernel_1(
   edge_list::kroneckerState{T, R}
   ) where T<:Integer where R<:Real
 
-  ij = edge_list.edge
-  ijw = edge_list.weight
-  cols = size(ij)[2]
+
+  ij1  = Vector{T}()
+  ij2  = Vector{T}()
+  ijw  = Vector{R}()
 
   # removing self Loop
-  for i in 1:cols
-    if(ij[1, i] == ij[2, i])
-      ijw[i] = zero(R)
+  cols = length(edge_list.weight)
+  sizehint!(ij1,cols)
+  sizehint!(ij2,cols)
+  sizehint!(ijw,cols)
+  index = 1
+  @simd for i in 1:cols
+    @inbounds if edge_list.edge[1,i] != edge_list.edge[2,i]
+      push!(ij1, edge_list.edge[1,i])
+      push!(ij2, edge_list.edge[2,i])
+      push!(ijw, edge_list.weight[i])
+      index += 1
     end
   end
 
   # Represent an undirected Graph
-  srcs = vcat(ij[1,:], ij[2, :])
-  dest = vcat(ij[2,:], ij[1, :])
+  srcs = vcat(ij1, ij2)
+  dest = vcat(ij2, ij1)
   weight = vcat(ijw, ijw)
 
   # Construct a sparse matrix and a SimpleWeightedGraph
