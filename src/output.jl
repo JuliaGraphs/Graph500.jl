d7c7d2121f80dbc976f54e26d9c39208767ea609
Divyansh <somm14divi@gmail.com>
2017-08-10 00:20:10 +0530

    Harmonic mean added

--- src/output.jl
+++ src/output.jl
@@ -1,9 +1,13 @@
 function output(result::Graph500Results)
+
+      # Benchmark info
       println("\n")
       println("Scale : ", result.SCALE)
       println("Number of vertices : ", ne(result.g))
       println("Number of Edges : ", nv(result.g))
       println("Number of Bfs : ", result.NBFS);
+
+      # kernel 1 Construction time
       println("Graph Construction Time : ", result.kernel_1_time)
       println("\n")
 
@@ -35,15 +39,20 @@ function output(result::Graph500Results)
       K2N = length(K2TEPS)
       S = summarystats(K2TEPS)
 
-
-
       # TEPS : Traversed edges per second
       println("BFS min TEPS : ", S.min)
       println("BFS first quartile TEPS : ", S.q25)
       println("BFS median TEPS : ", S.median)
       println("BFS third quartile TEPS : ", S.q75)
-      println("BFS max nedge : ", S.max)
-      println("BFS mean nedge : ", S.mean)
-    #   println("BFS standard deviation TEPS : ", )
+      println("BFS max TEPS : ", S.max)
+
+
+      # harmomnic mean of TEPS
+      k2tmp = 1.0 ./ K2TEPS
+      k2tmp = k2tmp - (1/harmmean(K2TEPS))
+      harmonic_std = (sqrt(sum(k2tmp.^2)) / (K2N-1)) * harmmean(K2TEPS)^2
+
+      println("BFS harmonic mean TEPS : ", harmmean(K2TEPS))
+      println("BFS harmonic standard deviation TEPS : ", harmonic_std)
 
   end
